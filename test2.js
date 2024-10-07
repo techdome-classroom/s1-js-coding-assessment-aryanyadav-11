a5b179f2c343186ef9eb7ebf9b4524d6698d739f
aryanyadav-11 <ay8575498@gmail.com>
2024-10-07 00:30:46 -0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,20 +1,20 @@
 const assert = require("assert");
 const decodeTheRing = require("./program2");
- 
+
 describe("Test cases for decodeTheRing function", function () {
-  it(`Returns true for "hello", "h*llo"`, function () {
+  it(`should return true for strings "hello" and "h*llo"`, function () {
     const result = decodeTheRing("hello", "h*llo");
-    assert.equal(result, true);
+    assert.strictEqual(result, true);
   });
- 
-  it(`Returns false for "secret", "sec?"`, function () {
+
+  it(`should return false for strings "secret" and "sec?"`, function () {
     const result = decodeTheRing("secret", "sec?");
-    assert.equal(result, false);
+    assert.strictEqual(result, false);
   });
- 
-  it(`Returns false for "codeword", "c*de"`, function () {
+
+  it(`should return false for strings "codeword" and "c*de"`, function () {
     const result = decodeTheRing("codeword", "c*de");
-    assert.equal(result, false);
+    assert.strictEqual(result, false);
   });
 });
 
